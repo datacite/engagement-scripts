eb151738d93c2a52299abf735aab9df6057ab4d3
lizkrznarich <liz.krznarich@datacite.org>
2021-06-18 09:46:35 -0500

    allow specifying which subscriber state to fetch

--- campaign-monitor/resend-confirmation.py
+++ campaign-monitor/resend-confirmation.py
@@ -8,13 +8,14 @@ from os import path
 
 CM_API_ENDPOINT = "https://api.createsend.com/api/v3.2/"
 SUBSCRIBERS_PATH = "subscribers/"
+OUTPUT_DIR = "tmp/"
 
 ## Iterate over file containing unconfirmed users and re-add subscribers to list, triggering confirmation email
 def add_subscribers(list_id, api_key, user, filename):
     url = CM_API_ENDPOINT + SUBSCRIBERS_PATH + list_id + ".json"
     with open(filename) as f:
         unconfirmed_subscribers = json.load(f)
-        for item in unconfirmed_subscribers['Results']:
+        for item in unconfirmed_subscribers:
             print("Resending to " + item['EmailAddress'])
             subscriber_dict= {
                 "EmailAddress": (item['EmailAddress']),
@@ -44,10 +45,11 @@ def main():
     parser.add_argument('-u', '--user', type=str)
     parser.add_argument('-f', '--filename', type=str)
     args = parser.parse_args()
-    if path.exists(args.filename):
-        add_unconfirmed_subscribers = add_subscribers(args.list_id, args.api_key, args.user, args.filename)
+    file_path = OUTPUT_DIR + args.filename
+    if path.exists(file_path):
+        add_unconfirmed_subscribers = add_subscribers(args.list_id, args.api_key, args.user, file_path)
     else:
-        print "File " + args.filename + " does not exist. Cannot add subscribers."
+        print "File " + file_path + " does not exist. Cannot add subscribers."
 
 if __name__ == '__main__':
   main()

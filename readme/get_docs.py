e1aeb0342c9286b8e19b4522cae44d6640752a22
Kelly Stathis <kelly.a.stathis@gmail.com>
2023-01-27 09:35:44 -0800

    write docs to CSV file

--- readme/get_docs.py
+++ readme/get_docs.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 def get_categories(apikey):
-    response = requests.get("https://dash.readme.com/api/v1/categories", auth=(apikey, ""))
+    response = requests.get("https://dash.readme.com/api/v1/categories?perPage=100", auth=(apikey, ""))
     return response.json()
 
 def get_docs_for_category(apikey, slug):
@@ -21,7 +21,16 @@ def get_doc(apikey, slug):
 def get_docs():
     load_dotenv()
     apikey = os.getenv("APIKEY")
-    docs_by_category = get_categories(apikey)
+    unsorted_docs_by_category = get_categories(apikey)
+    docs_by_category = []
+    i = 0
+    while i < len(unsorted_docs_by_category):
+        for j in unsorted_docs_by_category:
+            if j["order"] == i:
+                docs_by_category.append(j)
+                break
+        i +=1
+
     for category in docs_by_category:
         print("**** Category: {} ****".format(category["title"]))
         category_docs = get_docs_for_category(apikey, category["slug"])
@@ -36,12 +45,64 @@ def get_docs():
                     child_doc_details =  get_doc(apikey, child_doc["slug"])
                     child_doc["details"] = child_doc_details
 
-    # write json data to file
     # current time
     ct = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # write json data to file
     f = open("{}_readme_docs.json".format(ct), "w")
     f.write(json.dumps(docs_by_category))
     f.close()
 
+    return docs_by_category
+
+def get_doc_row(doc, category, parentDoc=""):
+    doc_row = {i: doc[i] for i in doc if i != "details" and i != "children"}
+    doc_row["categoryName"] = category["title"]
+    if parentDoc:
+        doc_row["parentDocName"] = parentDoc["title"]
+
+    if parentDoc:
+        doc_row["sortId"] = "#{:02d}.{:02d}.{:02d}".format(category["order"], parentDoc["order"], doc["order"])
+    else:
+        doc_row["sortId"] = "#{:02d}.{:02d}".format(category["order"], doc["order"])
+
+    for i in doc["details"]:
+        if i not in ["body", "body_html"]:
+            doc_row[i] = doc["details"][i]
+    return doc_row
+
+def format_docs(docs_by_category):
+    with open("readme_docs.csv", "w") as csvout:
+        header = ['type', 'sortId', 'title', 'categoryName', 'parentDocName', 'slug', 'hidden', 'isReference', 'order',
+                  'createdAt', 'updatedAt', 'link_external', 'link_url', 'reference', '_id', 'category', 'parentDoc',
+                  '__v', 'from_sync', 'sync', 'version', 'deprecated', 'project', 'id', 'details',
+                  'metadata', 'next', 'algolia', 'icon', 'updates', 'excerpt', 'sync_unique',
+                  'previousSlug', 'slugUpdatedAt', 'revision', 'pendingAlgoliaPublish',
+                  'user', 'isApi', 'githubsync', 'swagger',
+                  'isAPI', 'apiSetting', 'api']
+        writer = csv.DictWriter(csvout, fieldnames=header)
+        writer.writeheader()
+
+        for category in docs_by_category:
+            category_row = {i:category[i] for i in category if i!="docs"}
+            category_row["categoryName"] = category["title"]
+            category_row["sortId"] = "#{:02d}".format(category["order"])
+            writer.writerow(category_row)
+
+            for doc in category["docs"]:
+                doc_row = get_doc_row(doc, category)
+                writer.writerow(doc_row)
+
+                for child_doc in doc["children"]:
+                    child_doc_row = get_doc_row(child_doc, category, doc)
+                    writer.writerow(child_doc_row)
+
 if __name__ == '__main__':
-    get_docs()
+    load_dotenv()
+    json_docs_file = os.getenv("JSON_DOCS")
+    if json_docs_file:
+        with open(json_docs_file, 'r') as f:
+            docs_by_category = json.load(f)
+    else:
+        docs_by_category = get_docs()
+    format_docs(docs_by_category)
